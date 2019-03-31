141a89cb98734def1cf863320d1752e29007a2a1
zeus0789 <abhishek.tiwari3507@gmail.com>
2019-03-31 09:33:23 +0530

    30+ hours of work

---
+++ fsync.py
@@ -0,0 +1,21 @@
+from firebase.firebase import FirebaseApplication, FirebaseAuthentication
+import json
+
+SECRET = 'KhGkMigTi9aD4Vv4zsz8xISP2kU5I7rgq265DXiZ'
+DSN = 'https://hint-ec580.firebaseio.com'
+EMAIL = 'abhishek.tiwari3507@gmail.com'
+authentication = FirebaseAuthentication(SECRET,EMAIL, True, True)
+firebase = FirebaseApplication(DSN, authentication)
+
+def uploadarticle(idno, body, headline, author, downvote, upvote,score):
+    #changing score to str beacuse json is not float32 serialisable
+    data={'body' :body, 'author' :author, 'headline':headline,'ml_score':str(score), 'idno': idno,'downvote':downvote, 'upvote':upvote}
+    putpath="/articles/"+str(idno)
+    firebase.patch(putpath, data)
+
+def fetcharticle(idno):
+    data = firebase.get("/articles/"+str(idno), None)
+    return data
+
+# #Test
+# uploadarticle("2+", "hello hi byt", "HHHH", "HHHHHHHHHH")
---
+++ updownvote.py
@@ -0,0 +1,47 @@
+from firebase.firebase import FirebaseApplication, FirebaseAuthentication
+import json
+
+SECRET = 'KhGkMigTi9aD4Vv4zsz8xISP2kU5I7rgq265DXiZ'
+DSN = 'https://hint-ec580.firebaseio.com'
+EMAIL = 'abhishek.tiwari3507@gmail.com'
+authentication = FirebaseAuthentication(SECRET,EMAIL, True, True)
+firebase = FirebaseApplication(DSN, authentication)
+
+def upvote(idno):
+    upvote = firebase.get("/articles/"+idno+"/upvote/", None)
+    if(upvote == None):
+        firebase.patch("/articles/"+idno, {'upvote': '1'})
+    #print("/articles/"+idno+"/upvote")
+    #print(upvote)
+    try:
+        upvote = upvote+1
+    except TypeError:
+        upvote = 1
+    firebase.patch("/articles/"+idno, {'upvote': upvote})
+
+def downvote(idno):
+    downvote = firebase.get("/articles/"+idno+"/downvote/", None)
+    if(downvote == None):
+        firebase.patch("/articles/"+idno, {'downvote': '1'})
+    try:
+        downvote = downvote+1
+    except TypeError:
+        downvote = 1
+    firebase.patch("/articles/"+idno, {'downvote': downvote})
+
+def getfakeratio(idno):
+    upvote = firebase.get("/articles/"+idno+"/upvote/", None)   
+    downvote = firebase.get("/articles/"+idno+"/downvote/", None)
+    try:
+        fakeratio = downvote/(upvote+downvote)
+    except:
+        fakeratio = 0
+    print(fakeratio)
+    return fakeratio
+
+#Test
+#upvote("1")
+#upvote("2")
+#downvote("3")
+#getfakeratio("1")
+
